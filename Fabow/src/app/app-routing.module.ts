a25740115d4e0319d69ea62a9f3ee3d4bf678867
johch3n611u <401450650@gapp.fju.edu.tw>
2021-12-07 22:06:32 +0800

    test

--- Fabow/src/app/app-routing.module.ts
+++ Fabow/src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
         component: EditTaskComponent,
     },
     {
-        path: '**',
+        path: '***',
         redirectTo: 'tasks',
         pathMatch: 'full'
     },
